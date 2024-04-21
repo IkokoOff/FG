e2286107142d1e8a8d01f4d36eaa04011ee36eeb
IkokoOff <159687221+IkokoOff@users.noreply.github.com>
2024-04-21 09:38:57 +0200

    Add files via upload

---
+++ commands/help.js
@@ -0,0 +1,93 @@
+/*
+
+  ██████╗░████████╗██╗░░██╗           
+  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
+  ██████╔╝░░░██║░░░░╚███╔╝░          
+  ██╔══██╗░░░██║░░░░██╔██╗░          
+  ██║░░██║░░░██║░░░██╔╝╚██╗          
+  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
+
+   
+   # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
+   ## FOR HELP CONTACT ME ON DISCORD
+   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
+   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
+*/
+const { ApplicationCommandOptionType } = require('discord.js');
+const db = require("../mongoDB");
+
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder } = require('discord.js');
+const { ButtonStyle } = require('discord.js');
+
+module.exports = {
+  name: "help",
+  description: "Get information about bot and commands.",
+  permissions: "0x0000000000000800",
+  options: [],
+
+  run: async (client, interaction) => {
+    try {
+      const musicCommandsEmbed = new EmbedBuilder()
+        .setColor(client.config.embedColor)
+        .setTitle('🎸 **Music Commands**')
+        .addFields(
+          { name: '🎹 Play', value: 'Stream a song from a given link or text from sources' },
+          { name: '⏹️ Stop', value: 'Makes the bot stop playing music and leave the voice' },
+          { name: '📊 Queue', value: 'View and manage the song queue of this server' },
+          { name: '⏭️ Skip', value: 'Skip the current playing song' },
+          { name: '⏸️ Pause', value: 'Pause the currently playing song' },
+          { name: '▶️ Resume', value: 'Resume the current paused song' },
+          { name: '🔁 Loop', value: 'Toggle loop mode for queue and current song' },
+          { name: '🔄 Autoplay', value: 'Enable or disable autoplay [play random songs ]' },
+          { name: '⏩ Seek', value: 'Seek to a specific time in the current song' },
+          { name: '⏮️ Previous', value: 'Play the previous song in the queue' },
+          { name: '🔀 Shuffle', value: 'Shuffle the songs in queue' },
+          { name: '📃 playlist', value: 'manage the playlists' }
+        )
+        .setImage(`https://cdn.discordapp.com/attachments/1004341381784944703/1165201249331855380/RainbowLine.gif?ex=654f37ba&is=653cc2ba&hm=648a2e070fab36155f4171962e9c3bcef94857aca3987a181634837231500177&`); 
+
+      const basicCommandsEmbed = new EmbedBuilder()
+        .setColor(client.config.embedColor)
+        .setTitle('✨ **Basic Commands**')
+        .addFields(
+          { name: '🏓 Ping', value: "Check the bot's latency" },
+          { name: '🗑️ Clear', value: 'Clear the song queue of this server' },
+          { name: '⏱️ Time', value: 'Display the current song playback time' },
+          { name: '🎧 Filter', value: 'Apply filters to enhance the sound as you love' },
+           { name: '🎵 Now Playing', value: 'Display the currently playing song information' },
+          { name: '🔊 Volume', value: 'Adjust the music volume [ hearing at high volumes is risky ]' },
+        ) 
+       .setImage('https://cdn.discordapp.com/attachments/1211959285479772160/1229853930994012243/standard_3.gif?ex=663131ba&is=661ebcba&hm=ef7e774c9e243cf9c302e2a682b335213af16927ea1e21e1c0ec3829ec0a442c&')
+      const button1 = new ButtonBuilder()
+        .setLabel('Discord')
+        .setURL('https://discord.gg/cWr9xqxB5N')
+        .setStyle(ButtonStyle.Link);
+
+      const row = new ActionRowBuilder()
+        .addComponents(button1);
+
+      interaction.reply({
+        embeds: [musicCommandsEmbed, basicCommandsEmbed],
+        components: [row]
+      }).catch(e => {});
+    } catch (e) {
+      console.error(e);
+    }
+  },
+};
+
+/*
+
+  ██████╗░████████╗██╗░░██╗           
+  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
+  ██████╔╝░░░██║░░░░╚███╔╝░          
+  ██╔══██╗░░░██║░░░░██╔██╗░          
+  ██║░░██║░░░██║░░░██╔╝╚██╗          
+  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
+
+   
+   # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
+   ## FOR HELP CONTACT ME ON DISCORD
+   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
+   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
+*/
